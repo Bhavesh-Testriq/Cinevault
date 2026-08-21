ebd9d5fb32383d25f10ec82b4942ee2ac17b95ca
Bhavesh-Testriq <bhavesh.s.testriq2@gmail.com>
2026-08-21 17:18:45 +0530

    style: make application responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Carousel from "@/components/Carousel";
 import MovieCarousel from "@/components/MovieComponents/MovieCarousel";
-import { getPopular, getTopRated, getTrending, getUpcoming } from "@/lib/tmdb";
+import { getPopular, getTopRated, getTrending } from "@/lib/tmdb";
 
 export default async function Home() {
-  const [trending, popular,topRated] = await Promise.all([getTrending(), getPopular(), getTopRated()])
+  const [trending, popular, topRated] = await Promise.all([getTrending(), getPopular(), getTopRated()])
 
   return (
-    <main className="px-20 mb-10">
+    // <main className="mb-10 px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20">
+    <main className="mb-10 px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20">
       <Carousel movies={trending.results} />
 
       <MovieCarousel title="Trending" movies={trending.results} />
       <MovieCarousel title="Popular" movies={popular.results} />
       <MovieCarousel title="Top Rated" movies={topRated.results} />
-      {/* <MovieSection title="Upcoming" movies={upcoming.results} /> */}
     </main>
   );
 }
